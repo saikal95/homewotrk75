a0e2a504510013487a537b92482b6fa401a5499a
saikal <15051995sat>
2022-01-28 20:44:32 +0600

    the form has been prepared

---
+++ message-api/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const Vigenere = require('caesar-salad').Vigenere;
+const cors = require('cors');
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.post('/encode', async (req, res, next) => {
+  try{
+    const message = {
+      password: req.body.password,
+      message: req.body.message,
+    }
+    await res.send(Vigenere.Cipher(message.password).crypt(`<div>${message.message.encode}</div>`));
+
+  }catch (e) {
+    next(e);
+  }
+
+});
+
+
+app.post('/decode', async (req, res, next) => {
+
+  try{
+    const message = {
+      password: req.body.password,
+      message: req.body.message,
+    }
+    await res.send(Vigenere.Cipher(message.password).crypt(`<div>${message.message.decode}</div>`));
+
+  }catch(e) {
+    next(e);
+  }
+
+});
+app.listen(port, () => {
+  console.log('We are live on ' + port);
+});
